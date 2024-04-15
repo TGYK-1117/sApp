44fafb7d93928b0b4c1377877b2c4bcda2589a14
TGYK-1117 <nikolaussires@gmail.com>
2024-04-14 22:02:12 -0500

    set up react front end and linked to rails app

--- app/javascript/components/Landing.jsx
+++ app/javascript/components/Landing.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default () => (
-	<div>
-		<Link 
-			to="/list"
-			classname="btn btn-lg"
-			role="button"
-		>
-		View List
-		</Link>
+	<div className="vw-100 vh-100 d-flex align-items-center justify-content-center">
+		<div className="jumbotron jumbotron-fluid bg-transparent">
+			<Link 
+				to="/list"
+				className="btn btn-lg custom-button"
+				role="button"
+			>
+			View List
+			</Link>
+		</div>
 	</div>
 )
